bfbea3b42e49e20538094b4550d428f5058016ae
jonathang2560 <jonathang2560@gmail.com>
2023-02-10 21:46:57 -0800

    Initital Commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Save the scroll position on before unload event
+window.addEventListener('beforeunload', function() {
+    localStorage.setItem('scrollPos', window.scrollY);
+  });
+  
+  // Restore the scroll position on load event
+  window.addEventListener('load', function() {
+    let scrollPos = localStorage.getItem('scrollPos');
+    if (scrollPos) {
+      window.scrollTo(0, scrollPos);
+    }
+  });
