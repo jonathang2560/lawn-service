02a9259dd7de311603e0c6323a6ffe3e1b134aaa
jonathang2560 <jonathang2560@gmail.com>
2023-02-14 20:27:22 -0800

    More CSS and JS

--- js/main.js
+++ js/main.js
@@ -15,4 +15,64 @@ window.addEventListener('beforeunload', function() {
 
 
   // -----------live typing------------
- 
+// Set up an array to hold the texts to be typed
+const texts = ["Residential Properties", "Commercial Properties", "everything else!"];
+
+// Set up some variables to keep track of the current text and the current index
+let currentTextIndex = 0;
+let currentText = texts[currentTextIndex];
+let index = 0;
+let isDeleting = false;
+
+// Get a reference to the typing-text element
+const typingText = document.getElementById("typing-text");
+
+// Use setInterval to repeatedly update the text
+setInterval(() => {
+  // Get the current substring
+  const substring = currentText.substring(0, index);
+
+  // Update the text in the element
+  typingText.textContent = "We work with " + substring;
+
+  // Increment or decrement the index depending on whether we are typing or deleting
+  if (index < currentText.length && !isDeleting) {
+    index++;
+  } else {
+    isDeleting = true;
+    if (index > 0) {
+      index--;
+    } else {
+      isDeleting = false;
+      currentTextIndex = (currentTextIndex + 1) % texts.length;
+      currentText = texts[currentTextIndex];
+      setTimeout(() => {
+        isDeleting = false;
+      }, 2000); // Wait for 2 seconds before starting to delete
+    }
+  }
+}, 120);
+
+
+
+
+
+
+// -----------------scrolling----------------------
+
+// Select all navigation links
+const navLinks = document.querySelectorAll('nav a');
+
+// Loop through each link and add an event listener
+navLinks.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault(); // prevent default link behavior
+    const sectionID = this.getAttribute('href'); // get the ID of the target section
+    const section = document.querySelector(sectionID); // select the target section
+    const sectionPosition = section.offsetTop; // get the position of the target section
+    window.scrollTo({
+      top: sectionPosition,
+      behavior: 'smooth'
+    }); // smooth scroll to the target section
+  });
+});
